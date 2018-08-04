8f39d390b7e3399df3c588650dafa7475ffdeba6
SandraCGarcia <sandra.gcanete@gmail.com>
2018-08-04 16:42:11 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//ejercicio1 2.1//
+
+//alert('Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente.');
+
+//ejercicio2 2.1
+
+// alert('Hola, mi nombre es Sandra');
+// alert('Me encanta lo que estoy haciendo');
+// alert('I love javeScript 4ever');
+
+//ejercicio3 2.1
+
+//alert('Toral kiwis = ' + (2*5) + ' €' + 'Total peras = ' + (3*2) + ' €' + 'Total uvas = ' + (4/2) + '€');
+
+//ejercicio4 2.1
+
+// var total= (128-2)/9;
+//
+// alert('Paga cada uno ' + total + '€');
+// alert('Ana paga ' + (total + 2) + '€');
+
+//ejercicio5 2.1
+
+// var resto= 2018%4;
+//
+// alert(resto + 2018);
+
+//ejercicio6 2.1
+
+// var edad = prompt('¿Qué edad tienes?');
+//
+// alert('Has vivido ' + ((edad*365)*24) + ' horas');
